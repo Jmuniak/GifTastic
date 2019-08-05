cd63aad4daff65ac59700378f2b6e196776fd74c
Jeremy Muniak <Jeremy.e.Muniak@gmail.com>
2019-08-05 18:31:29 -0400

    Functional Homework, needs readme and CSS

---
+++ app.js
@@ -0,0 +1,72 @@
+$(function () {
+    // Short Document Ready
+
+    // array of topics
+    let topics = ["cowboy beepop", "star citizen", "snowboarding", "nick offerman", "iron man", "letterkenny", "guardians of the galaxy"]
+
+    console.log(topics);
+    appendButtons();
+
+    // Whenever a user clicks the click button
+    // Get the new topic from the form
+    $("#topicForm").submit(function (event) {
+        event.preventDefault();
+        if ($("#topicInput").val() === "") {
+            alert("You need to enter a topic first")
+        } else {
+            let newTopicText = $("#topicInput").val();
+            console.log(newTopicText);
+            topics.push(newTopicText);
+            console.log(topics);
+            $("#buttons").empty();
+            appendButtons();
+        }
+    });
+
+    // Append topics to buttons div
+    function appendButtons() {
+        // create a  button with a dataValue that matches the array index text
+        topics.forEach(newTopicText => {
+            let newButton = $("<button>")
+                .addClass("button secondary")
+                .attr("dataValue", newTopicText)
+                .text(newTopicText)
+            $("#buttons").append(newButton);
+        })
+    };
+
+
+    // Button Click GET GIF function
+    $(".button").on("click", function () {
+        let topic = $(this).attr("dataValue");
+        let queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+            topic + "&api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9&limit=10";
+        console.log("ajax start");
+        $.ajax({
+                url: queryURL,
+                method: "GET"
+            })
+            .then(function (response) {
+                let results = response.data;
+
+                for (let i = 0; i < results.length; i++) {
+                    let gifDiv = $("<div>");
+
+                    let rating = results[i].rating;
+
+                    let p = $("<p>").text("Rating: " + rating);
+
+                    let topicImage = $("<img>");
+                    topicImage.attr("src", results[i].images.fixed_height.url);
+
+                    gifDiv.prepend(p);
+                    gifDiv.prepend(topicImage);
+
+                    $("#gifs-appear-here").prepend(gifDiv);
+                }
+            });
+        console.log("ajax done");
+    });
+
+
+});
