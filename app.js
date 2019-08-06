de34d0ee1da32d7d7f94de1c4e7e636bb30c6e22
Jeremy Muniak <Jeremy.e.Muniak@gmail.com>
2019-08-06 11:00:38 -0400

    Fixed a bug where gifs where appeneded too many times because of the click event being in the for loop

--- app.js
+++ app.js
@@ -24,6 +24,22 @@ $(function () {
         }
     });
 
+
+
+    //     // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
+    //     var state = $(this).attr("data-state");
+    //     // If the clicked image's state is still, update its src attribute to what its data-animate value is.
+    //     // Then, set the image's data-state to animate
+    //     // Else set src to the data-still value
+    //     if (state === "still") {
+    //         $(this).attr("src", $(this).attr("data-animate"));
+    //         $(this).attr("data-state", "animate");
+    //     } else {
+    //         $(this).attr("src", $(this).attr("data-still"));
+    //         $(this).attr("data-state", "still");
+    //     }
+    // });
+
     // Append topics to buttons div
     function appendButtons() {
         // create a  button with a dataValue that matches the array index text
@@ -33,42 +49,39 @@ $(function () {
                 .attr("dataValue", newTopicText)
                 .text(newTopicText)
             $("#buttons").append(newButton);
-            // Button Click GET GIF function
-            // $(document.getElementsByClassName())  try this instead
-
-            $(".button").on("click", function () {
-                let topic = $(this).attr("dataValue");
-                let queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-                    topic + "&api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9&limit=10";
-                console.log("ajax start");
-                $.ajax({
-                        url: queryURL,
-                        method: "GET"
-                    })
-                    .then(function (response) {
-                        let results = response.data;
-
-                        for (let i = 0; i < results.length; i++) {
-                            let gifDiv = $("<div>");
-
-                            let rating = results[i].rating;
-
-                            let p = $("<p>").text("Rating: " + rating);
-
-                            let topicImage = $("<img>");
-                            topicImage.attr("src", results[i].images.fixed_height.url);
-
-                            gifDiv.prepend(p);
-                            gifDiv.prepend(topicImage);
-
-                            $("#gifs-appear-here").prepend(gifDiv);
-                        }
-                    });
-                console.log("ajax done");
-            });
-
-
         })
+        // Button Click GET GIF function
+        $(".button").on("click", function () {
+            let topic = $(this).attr("dataValue");
+            let queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+                topic + "&api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9&limit=10";
+            console.log("ajax start");
+            $.ajax({
+                    url: queryURL,
+                    method: "GET"
+                })
+                .then(function (response) {
+                    console.log(response);
+                    let results = response.data;
+
+                    for (let i = 0; i < 5; i++) {
+                        let gifDiv = $("<div>");
+
+                        let rating = results[i].rating;
+
+                        let p = $("<p>").text("Rating: " + rating);
+
+                        let topicImage = $("<img>");
+                        topicImage.attr("src", results[i].images.fixed_height.url);
+
+                        gifDiv.prepend(p);
+                        gifDiv.prepend(topicImage);
+
+                        $("#gifs-appear-here").prepend(gifDiv);
+                    }
+                });
+            console.log("ajax done");
+        });
     };
 
 
