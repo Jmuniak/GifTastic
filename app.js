ed0d57de5695fc24eca21120101581ec24e2f58d
Jeremy Muniak <Jeremy.e.Muniak@gmail.com>
2019-10-24 17:05:16 -0400

    Made some styling changes with bootstrap. Starting to work on the ajax call being more portable so it can be used upon the new topic being added or when a topic button gets clicked.

--- app.js
+++ app.js
@@ -2,12 +2,12 @@ $(function () {
     // Short Document Ready
 
     // array of topics
-    let topics = ["Cowboy Beepop", "Sci-Fi", "Snowboarding", "Nick Offerman", "Iron Man", "Letterkenny", "Guardians of the Galaxy"]
+    let topics = ["Boba Fett", "Sci-Fi", "Snowboarding", "Nick Offerman", "Iron Man", "Letterkenny", "Guardians of the Galaxy", "R2D2", "Treasure Planet", "Hover Cars", "Avengers", "Nature", "Bob Ross"]
 
     console.log(topics);
     appendButtons();
 
-    // Whenever a user clicks the click button
+
     // Get the new topic from the form
     $("#topicForm").submit(function (event) {
         event.preventDefault();
@@ -21,59 +21,62 @@ $(function () {
             $("#buttons").empty();
             $("#topicForm")[0].reset();
             appendButtons();
+            runAjax(newTopicText)
         }
     });
-
-
-
-
 
     // Append topics to buttons div
     function appendButtons() {
         // create a  button with a dataValue that matches the array index text
         topics.forEach(newTopicText => {
-            let newButton = $("<button>")
-                .addClass("button secondary")
+            let newButton = $(`<button type="button" class="btn btn-outline-dark">`)
                 .attr("dataValue", newTopicText)
                 .text(newTopicText)
             $("#buttons").append(newButton);
-        })
+        });
+    };
 
-    // Button Click GET GIF function
-    $(".button").on("click", function () {
-        let topic = $(this).attr("dataValue");
-        let queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-            topic + "&api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9&limit=10";
-        console.log("ajax start");
-        $.ajax({
-            url: queryURL,
-            method: "GET"
-        })
-            .then(function (response) {
-                console.log(response);
-                let results = response.data;
+    function runAjax(data) {
+        
 
-                for (let i = 0; i < 5; i++) {
-                    let gifDiv = $("<div>");
-                    let rating = $("<p>").text("Rating: " + results[i].rating);
-                    let topicImage = $(`<img class="topicImg" src=${results[i].images.downsized_still.url} src-alt=${results[i].images.downsized.url}></img>`);
+    }
 
-                    gifDiv.prepend(rating);
-                    gifDiv.prepend(topicImage);
-                    $("#gifs-appear-here").prepend(gifDiv);
-                }
-                $(".pics").on("click", function () {
-                    console.log("onclick")
-                    console.log(this)
-                    let temp = $(this).attr('src')
-                    $(this).attr("src", $(this).attr("src-alt"))
-                    $(this).attr("src-alt", temp)
+        // Button Click GET GIF function
+        $(".btn").on("click", function () {
+            $("#gifs-appear-here").empty();
+            // ajax call function
+            let topic = $(this).attr("dataValue");
+            let queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+                topic + "&api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9&limit=15";
+            console.log("ajax start");
+            $.ajax({
+                url: queryURL,
+                method: "GET"
+            })
+                .then(function (response) {
+                    console.log(response);
+                    let results = response.data;
+
+                    for (let i = 0; i < 15; i++) {
+                        let gifDiv = $(`<div class="col-md-3">`);
+                        let rating = $(`<p class="gifRating col-sm-6">`).text("Rating: " + results[i].rating);
+                        let topicImage = $(`<img class="topicImg" src=${results[i].images.downsized_still.url} src-alt=${results[i].images.downsized.url}></img>`);
+
+                        gifDiv.prepend(rating);
+                        gifDiv.prepend(topicImage);
+                        $("#gifs-appear-here").prepend(gifDiv);
+                    }
+                    $(".topicImg").on("click", function () {
+                        console.log("onclick")
+                        console.log(this)
+                        let temp = $(this).attr('src')
+                        $(this).attr("src", $(this).attr("src-alt"))
+                        $(this).attr("src-alt", temp)
+                    });
                 });
-            });
-        console.log("ajax done");
+            console.log("ajax done");
+        });
 
-    });
-};
 
     // document ready function
 });
