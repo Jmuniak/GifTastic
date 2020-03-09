d0c69fad836329f5755222284d9766162413e676
Jeremy Muniak <Jeremy.e.Muniak@gmail.com>
2020-03-09 19:40:33 -0400

    Updated Readme, minor styling, added footer

--- assets/app.js
+++ assets/app.js
@@ -1,10 +1,10 @@
 $(function () {
     // Short Document Ready
 
-    // array of topics
-    let topics = ["Puppies", "Boba Fett", "Sci-Fi", "Snowboarding", "Nick Offerman", "Iron Man", "Letterkenny", "Guardians of the Galaxy", "R2D2", "X-wing", "Hover Cars", "Avengers", "Nature", "Bob Ross"]
+    // Array of topics
+    let topics = ["Puppies", "Snowboarding", "Sci-Fi", "Iron Man", "Guardians of the Galaxy", "R2D2", "X-wing", "Nature", "Bob Ross"]
 
-    console.log(topics);
+    // console.log(topics);
     appendButtons();
 
     // Get the new topic from the form
@@ -14,9 +14,9 @@ $(function () {
             alert("You need to enter a topic first")
         } else {
             let newTopicText = $("#topicInput").val();
-            console.log(newTopicText);
+            // console.log(newTopicText);
             topics.push(newTopicText);
-            console.log(topics);
+            // console.log(topics);
             $("#buttons").empty();
             $("#topicForm")[0].reset();
             runAjax(newTopicText)
@@ -40,12 +40,12 @@ $(function () {
         // ajax call function
         let topic = data;
         let queryURL = `https://api.giphy.com/v1/gifs/search?q=${topic}&api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9&limit=15`;
-        console.log("ajax start", queryURL);
+        // console.log("ajax start", queryURL);
         $.ajax({
             url: queryURL,
             method: "GET"
         }).then(function (response) {
-            console.log(response);
+            // console.log(response);
             let results = response.data;
 
             results.forEach(element => {
@@ -58,24 +58,23 @@ $(function () {
             });
             // 'pause play' functionality for gif images. Using jquery and a bubble switch
             $(".topicImg").on("click", function () {
-                console.log("onclick")
-                console.log(this)
+                // console.log("onclick")
+                // console.log(this)
                 let temp = $(this).attr('src')
                 $(this).attr("src", $(this).attr("src-alt"))
                 $(this).attr("src-alt", temp)
             });
         });
-        console.log("ajax done");
-
+        // console.log("ajax done");
     };
 
     // Button Click GET GIF function
     $(document).on("click", ".topicBtn", function () {
-        console.log(this);
+        // console.log(this);
         let btnDataVal = $(this).attr("dataValue");
         runAjax(btnDataVal);
     });
 
 
-    // document ready function
+    // end document ready function
 });
